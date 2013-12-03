9c49a276a00407c08ba89db82d28729c31da304f
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-02 22:37:46 -0500

    add basic post and user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Vespa::Application.routes.draw do
+  resources :posts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
