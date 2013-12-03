b1766fa4a3cef27a66410de427feae2858b31130
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-02 23:01:43 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Vespa::Application.routes.draw do
   devise_for :users
   resources :posts
 
-  resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
