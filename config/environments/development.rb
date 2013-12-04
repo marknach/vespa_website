5dbba201ebaaeb4576dd707c820f9471ae666977
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-03 22:07:54 -0500

    fix logout and only show edit/delete if you wrote post

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ Vespa::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.serve_static_assets = true
 end
