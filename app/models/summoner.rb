88199629fae210af461c774ba31d2a59a100ab64
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-12 20:36:42 -0500

    added form for adding summoner

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,2 +1,3 @@
 class Summoner < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
+  include RiotHelper 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   has_many :summoners
+
   def is_admin?
     return self.email == "admin@vt.edu" 
   end
+#  private
+#  def update_summoner_info(summoner_id)
+#    puts "updating #{summoner_id}
+#  end
 end
