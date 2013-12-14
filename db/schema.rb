d8f6666b07fc2b7beffc012edce6ac8978b05d3b
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-14 08:42:45 -0500

    add image_url column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213050826) do
+ActiveRecord::Schema.define(version: 20131214134213) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20131213050826) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "summoner_id"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
