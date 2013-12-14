b97d54563a68dcfbcb81c62f3fa4f64fecd6ae12
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-14 14:45:05 -0500

    refactor db calls to eager load users instead of in view code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @recent_posts = Post.order(created_at: :desc).take(3) 
+    @recent_posts = Post.includes(:user).order(created_at: :desc).take(3) 
     @upcoming_events = Event.where(date: Date.today..1.weeks.from_now) 
   end
  
