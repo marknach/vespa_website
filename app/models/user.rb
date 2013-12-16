5eb1b32a95b6aef87070686b363a9fe51a25f8ef
Mark Nachazel <marknach@vt.edu>
2013-12-16 09:05:47 -0500

    move RiotHelper calls into summoner class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
-class User < ActiveRecord::Base
-  include RiotHelper 
+class User < ActiveRecord::Base 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   has_many :posts
   has_many :summoners
 
@@ -14,14 +14,13 @@ class User < ActiveRecord::Base
   def to_s
     self.name.presence || self.email
   end
+  
   private
 
   def add_summoner(summoner_name) 
-    summoner = Summoner.find_by(name: summoner_name)
-    summoner = self.summoners..create(find_summoner_by_name(summoner_name)) unless summoner
-  end
-
-  def update_summoner_info(summoner_id)
-    puts "updating #{summoner_id}"
-  end
+    puts summoner_name
+   # summoner = Summoner.find_by(name: summoner_name)
+   # summoner = self.summoners.create(find_summoner_by_name(summoner_name)) unless summoner
+   # self.summoners << summoner
+  end 
 end
