4e55220a368fad8b5a46b4db3df6154d1a425e23
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-12-25 00:50:01 -0500

    assets + first test scaffold

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,10 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  describe Post do
+    it 'should truncate long post' do
+      
+    end
+
+  end
 end
