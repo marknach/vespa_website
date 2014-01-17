f095ce4c7170434ed09e99e1c7361e683898ff23
Mark Nachazel <mark.nachazel@altamiracorp.com>
2014-01-16 22:17:55 -0500

    think i finally got it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,12 @@ Vespa::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
   
-  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf')
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -61,6 +60,7 @@ Vespa::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
