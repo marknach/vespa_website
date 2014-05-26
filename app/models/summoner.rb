63da17a06ac75eb1026722aaa49fe4c3037e4c60
Mark Nachazel <mark.nachazel@altamiracorp.com>
2014-05-26 15:00:21 -0400

    remove reference to old riothelper

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,5 +1,4 @@
 class Summoner < ActiveRecord::Base
-  include RiotHelper
   belongs_to :user
   validates :name, presence: true
   validates :summonerLevel, :numericality => { :only_integer => true, :greater_than => 0, :less_than_or_equal_to => 30 }
