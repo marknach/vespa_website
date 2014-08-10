533177ffb88838ecc72b66bbcae87d131030ea19
Mark Nachazel <marknach@vt.edu>
2014-08-10 08:41:38 -0400

    add feed of recent posts by user to their profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,4 @@ class PagesController < ApplicationController
     @recent_announcements = Announcement.includes(:user).order(created_at: :desc).take(3) 
     @upcoming_events = Event.where(date: Date.today..1.weeks.from_now) 
   end
- 
-
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @recent_announcements = Announcement.where(user: @user).order(created_at: :desc).take(3) 
   end
 end
