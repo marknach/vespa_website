6ce235939ea8d423d1ca1ed18dff086cf49b90cc
Mark Nachazel <marknach@vt.edu>
2014-08-23 13:08:49 -0400

    small tweaks to profile page - make picture bigger

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include Gravtastic
-  gravtastic :size => 128
+  gravtastic :size => 512
          
   has_many :posts
   has_many :summoners
