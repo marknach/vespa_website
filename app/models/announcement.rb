173ee36fa02ccdb1f6ca3e921504a25b29d0cd42
Mark Nachazel <Mark.Nachazel@altamiracorp.com>
2015-02-16 10:59:18 -0800

    Update (read more) to only appear when there is more

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,11 +1,13 @@
 class Announcement < ActiveRecord::Base
   include ActionView::Helpers::TextHelper
+	include Rails.application.routes.url_helpers
   belongs_to :user
 
 	@@md
 
   def preview
-    Markdown.new(truncate(self.content, length: 300, separator: ' ')).to_html
+	  read_more = "[... (read more)](#{announcement_path(self)})"
+    Markdown.new(truncate(self.content, length: 300, separator: "\n", omission: read_more)).to_html
   end
 	
 	def html_content
